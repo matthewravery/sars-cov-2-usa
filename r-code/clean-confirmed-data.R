64154d1ef5fdeee9524d757300b898d85e5ea349
Matthew Avery <matthewravery@gmail.com>
2020-03-19 21:21:49 -0600

    Scripts for cleaning data

---
+++ r-code/clean-confirmed-data.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(janitor)
+library(lubridate)
+
+
+# Read in data
+tb <- read_csv("../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv") %>% 
+  filter(`Country/Region` == "US",
+         `Province/State` != c("Diamond Princess"),
+         `Province/State` != c("Grand Princess")) %>% 
+  mutate(notstate = str_detect(`Province/State`, ",")) %>% 
+  filter(!notstate) %>% 
+  select(-notstate, -Lat, -Long, -`Country/Region`) %>% 
+  adorn_totals() %>% 
+  as_tibble() 
+
+
+# Tidy and add dates
+tb <- tb %>% 
+  pivot_longer(cols = -`Province/State`, names_to = "Date", values_to = "Confirmed") %>% 
+  mutate(Date = as_date(map_dbl(Date, mdy))) %>% 
+  filter(Date > mdy("March 9, 2020"))
+
+
+
+states <- tb$`Province/State`[tb$`Province/State` != "Total"] %>% unique
+
+statepop <- read_csv("data/nst-est2019-alldata.csv") %>% 
+  select(NAME, POPESTIMATE2019) %>% 
+  filter(NAME %in% states) %>% 
+  rename(`Province/State` = NAME,
+         population = POPESTIMATE2019)
+
+out <- left_join(tb, statepop) %>% 
+  mutate(`Confirmed per million residents` = 10^6 * Confirmed / population)
+
+
+write_csv(out, "shiny/sars-cov-2-usa-by-state/data/us-confirmed-cleaned.csv")
+write_csv(out, "data/us-confirmed-cleaned.csv")
---
+++ r-code/run-cleaning-scripts.R
@@ -0,0 +1,3 @@
+source("r-code/clean-confirmed-data.R")
+source("r-code/clean-deaths-data.R")
+source("r-code/clean-recovered-data.R")
