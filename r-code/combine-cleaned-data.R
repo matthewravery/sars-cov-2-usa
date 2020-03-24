11abff4094600521da4b08d2089fff4947603847
Matthew Avery <matthewravery@gmail.com>
2020-03-23 20:26:36 -0600

    Added population scaling back in to data

--- r-code/combine-cleaned-data.R
+++ r-code/combine-cleaned-data.R
@@ -4,9 +4,19 @@ library(lubridate)
 # source("r-code/clean-data-20200310-through-20200322.R")
 source("r-code/clean-data-20200323-through-today.R")
 
-out <- bind_rows(read_csv("data/cleaned-data-20200310-through-20200322.csv"), 
+tb <- bind_rows(read_csv("data/cleaned-data-20200310-through-20200322.csv"), 
           read_csv("data/cleaned-data-20200323-through-today.csv")) 
 
+statepop <- read_csv("data/nst-est2019-alldata.csv") %>% 
+  select(NAME, POPESTIMATE2019) %>% 
+  filter(NAME %in% states) %>% 
+  rename(`Province/State` = NAME,
+         population = POPESTIMATE2019)
+
+out <- left_join(tb, statepop) %>% 
+  mutate(`Confirmed per million residents` = 10^6 * Confirmed / population,
+         `Deaths per million residents` = 10^6 * Deaths / population,
+         `Recoveries per million residents` = 10^6 * Recovered / population)
 
 out %>% 
   write_csv("data/cleaned-data-all-series.csv")
