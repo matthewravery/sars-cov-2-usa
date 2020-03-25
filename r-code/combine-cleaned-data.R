55fe07610c64c9125d8d8e083d689a8801c7f217
Matthew Avery <matthewravery@gmail.com>
2020-03-25 16:14:38 -0600

    Added explicit method for getting state

--- r-code/combine-cleaned-data.R
+++ r-code/combine-cleaned-data.R
@@ -1,11 +1,14 @@
 library(tidyverse)
 library(lubridate)
 
-# source("r-code/clean-data-20200310-through-20200322.R")
+source("r-code/clean-data-20200310-through-20200322.R")
 source("r-code/clean-data-20200323-through-today.R")
 
 tb <- bind_rows(read_csv("data/cleaned-data-20200310-through-20200322.csv"), 
-          read_csv("data/cleaned-data-20200323-through-today.csv")) 
+          read_csv("data/cleaned-data-20200323-through-today.csv")) %>% 
+  arrange(Date)
+
+states <- tb$`Province/State` %>% unique
 
 statepop <- read_csv("data/nst-est2019-alldata.csv") %>% 
   select(NAME, POPESTIMATE2019) %>% 
@@ -16,7 +19,8 @@ statepop <- read_csv("data/nst-est2019-alldata.csv") %>%
 out <- left_join(tb, statepop) %>% 
   mutate(`Confirmed per million residents` = 10^6 * Confirmed / population,
          `Deaths per million residents` = 10^6 * Deaths / population,
-         `Recoveries per million residents` = 10^6 * Recovered / population)
+         `Recoveries per million residents` = 10^6 * Recovered / population) %>% 
+  filter(`Province/State` %in% statepop$`Province/State`)
 
 out %>% 
   write_csv("data/cleaned-data-all-series.csv")
